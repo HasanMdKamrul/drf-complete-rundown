b0310fcc185827db00de1a4c54f5adefe4ed9ca6
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-02-01 04:50:27 +0100

    first api added without db

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -1,9 +1,13 @@
 # Trying the api with the raw python client and requests
 import requests
 
-endpoint = "https://jsonplaceholder.typicode.com/posts"
-
+# endpoint = "https://jsonplaceholder.typicode.com/posts/1"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
 response = requests.get(endpoint)
 
-print(response.text) # Raw jeson response will be seen on the console
+# print(response.text)
+print(response.json()["message"]) # Raw jeson response will be seen on the console
+
+print(response.status_code)
 
