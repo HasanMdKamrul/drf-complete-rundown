e868d9845e3761debffb3b32d261ea41ad9b18e0
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-02-02 02:02:13 +0100

    converted the api to rest_framework api

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -4,10 +4,9 @@ import requests
 # endpoint = "https://jsonplaceholder.typicode.com/posts/1"
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
-response = requests.get(endpoint)
+response = requests.get(endpoint,params={"abc":123},json={"query":"Hello World"})
 
 # print(response.text)
-print(response.json()["message"]) # Raw jeson response will be seen on the console
+print(response.json()) # Raw jeson response will be seen on the console
 
-print(response.status_code)
-
+# print(response.status_code)
