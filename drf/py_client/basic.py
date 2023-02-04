ef55bdcbf1556f3bc6b6f501c28a42c545e821bc
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-02-04 18:44:54 +0100

    Optinally sending data if don't want to send data in a specific field

--- drf/py_client/basic.py
+++ drf/py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 # endpoint = "https://jsonplaceholder.typicode.com/posts/1"
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
-response = requests.get(endpoint,params={"abc":123},json={"query":"Hello World"})
+response = requests.post(endpoint,json={"title":"Hello World","content":None, "price":12.33})
 
 # print(response.text)
 print(response.json()) # Raw jeson response will be seen on the console
