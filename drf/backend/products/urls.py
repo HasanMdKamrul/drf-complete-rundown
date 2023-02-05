5bc06143e23983b272055f6d323b7f74fcfabcb1
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-02-05 01:53:34 +0100

    Create view added with the perform create changes

--- drf/backend/products/urls.py
+++ drf/backend/products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import product_detail_api_view
+from .views import product_detail_api_view,product_create_api_view
 
 urlpatterns = [
     path('<int:pk>/', product_detail_api_view, name='product-detail'),
+    path('', product_create_api_view, name='product-create'),
 ]
--- drf/backend/products/views.py
+++ drf/backend/products/views.py
@@ -1,14 +1,30 @@
-from rest_framework.generics import  RetrieveAPIView
+from rest_framework.generics import  RetrieveAPIView, CreateAPIView
 
 from django.http import JsonResponse
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.permissions import IsAuthenticated,AllowAny
 
 
 class ProductDetailApiView(RetrieveAPIView):
+    permission_classes = [AllowAny]
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    
-
 
 product_detail_api_view = ProductDetailApiView.as_view()
+
+class ProductCreateApiView(CreateAPIView):
+    permission_classes = [AllowAny]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+        
+product_create_api_view = ProductCreateApiView.as_view()
+
+
