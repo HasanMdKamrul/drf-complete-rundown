d18fbb9e371a00802d8676e3c2bec88150632a02
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-02-06 05:18:18 +0100

    function abased view explored

--- drf/backend/products/urls.py
+++ drf/backend/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import product_detail_api_view,product_create_api_view,product_list_create_api_view,product_update_api_view
+from .views import product_detail_api_view,product_create_api_view,product_list_create_api_view,product_update_api_view,get_all_operations
 
 urlpatterns = [
     path('<int:pk>/', product_detail_api_view, name='product-detail'),
     path('<int:pk>/update/', product_update_api_view, name='product-update'),
     path('', product_list_create_api_view, name='product-create'),
+    path('alloptions/', get_all_operations, name='product-all-options'),
+    path('alloptions/<int:pk>/', get_all_operations, name='product-all-options-id'),
 ]
--- drf/backend/products/views.py
+++ drf/backend/products/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from .models import Product
 from .serializers import ProductSerializer
 from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 class ProductDetailApiView(RetrieveAPIView):
@@ -49,4 +51,56 @@ class ProductListCreateApiView(ListCreateAPIView):
         
 product_list_create_api_view = ProductListCreateApiView.as_view()
 
-
+@api_view(['GET','POST','PUT','DELETE'])
+def get_all_operations(request,*args,**kwargs):
+    # ** get all products
+    if request.method == 'GET':
+        pk = kwargs.get('pk') 
+        if pk is not None:
+            product = Product.objects.filter(pk=pk)
+            serializer = ProductSerializer(product,many=True)
+            if serializer.data:
+                return Response(serializer.data)
+            return Response({"message":"Product not found"},status=404)     
+        else:
+            products = Product.objects.all()
+            serializer = ProductSerializer(products,many=True)
+            return Response(serializer.data)     
+        
+    # ** create new product
+    
+    if request.method == 'POST':
+        new_product = request.data
+        serializer = ProductSerializer(data=new_product)
+        if serializer.is_valid():
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+            if content is None:
+                content = title
+                serializer.save(content=content)
+                return Response(serializer.data)
+            
+        return Response(serializer.errors)
+    
+    # # ** update product
+    
+    if request.method == 'PUT':
+        updated_data = request.data
+        pk = kwargs.get('pk')
+        object_to_update = Product.objects.filter(pk=pk).first()
+        serializer = ProductSerializer(object_to_update,updated_data)
+        
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)  
+        return Response(serializer.errors)
+        
+        
+                
+            
+       
+        
+       
+        # return JsonResponse(serializer.errors)
+        
+    
