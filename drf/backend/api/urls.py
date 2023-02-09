9ac6573f6489fa60ecc983f3ea2a73ec768cd976
Md Kamrul Hasan <kamrulhasan@iut-dhaka.edu>
2023-02-09 02:34:28 +0100

    custom auth token with custom json response added

--- drf/backend/api/urls.py
+++ drf/backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
 
+# from rest_framework.authtoken.views import obtain_auth_token
+from .authentication import CustomAuthToken
 from .views import api_home, get_all_products
 
 urlpatterns = [
     path('', api_home, name="home"),
-    path('auth/', obtain_auth_token, name="auth" ),
+    path('auth/', CustomAuthToken.as_view(), name="auth" ),
     path('allproducts', get_all_products, name="allproducts")
 ]
